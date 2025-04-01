0fd96f6e22a3e415450f0ddeff45c4075770ade8
Brice McKeel <Bricemck@gmail.com>
2025-04-01 17:26:21 -0400

    working MVC

--- controllers/pokedex.js
+++ controllers/pokedex.js
@@ -1,27 +1,65 @@
 const dexCon = require('../models/pokedex');
+
+const home = async (req, res) => {
+    res.render('index.ejs');
+};
+
 const index = async (req, res) => {
-    const seenPokemon = await Dex.find();
+    const seenPokemon = await dexCon.find();
     res.render("pokedex/index.ejs", { Dex: seenPokemon });
 };
+
 const show = async (req, res) => {
-    const pokemonDetailPage = await Dex.findById(req.params.DexId);
+    const pokemonDetailPage = await dexCon.findById(req.params.DexId);
     res.render(`pokedex/show.ejs`, { Dex: pokemonDetailPage });
 };
-// const create = createLogic;
-// const update = updateLogic;
-// const delete = deleteLogic;
-// const showNewForm = newFormLogic;
-// const edit = editLogic;
 
+const showNewForm = async (req, res) => {
+    res.render("pokedex/new.ejs");
+};
 
+const create = async (req, res) => {
+    // req.body.canEvolve = req.body.canEvolve ==="on";
+        if (req.body.canEvolve === "on") {
+            req.body.canEvolve = true;
+        } else {
+            req.body.canEvolve = false;
+        }
+        await dexCon.create(req.body);
+        res.redirect("/pokedex");
+};
 
+const remove = async (req, res) => {
+    await dexCon.findByIdAndDelete(req.params.DexId);
+    res.redirect('/pokedex');
+};
+
+const edit = async (req, res) => {
+    const editEntry = await dexCon.findById(req.params.DexId);
+    console.log(editEntry);
+    res.render("pokedex/edit.ejs", {
+        dexCon: editEntry
+    });
+};
+
+const update = async (req, res) => {
+    if (req.body.canEvolve === "on") {
+        req.body.canEvolve = true;
+    } else {
+        req.body.canEvolve = false;
+    }
+    await dexCon.findByIdAndUpdate(req.params.DexId, req.body);
+    res.redirect(`/pokedex/${req.params.DexId}`)
+};
 
 module.exports = {
+    home,
     index,
     show,
-    // create,
-    // update,
-    // delete,
-    // showNewForm,
-    // edit,
+    create,
+    update,
+    remove,
+    showNewForm,
+    edit,
 };
+
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Variables & Modules
+// Variables 
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
@@ -6,80 +6,110 @@ const mongoose = require('mongoose')
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 
-
-// Load Express
+// Load Express & Mongoose
 const app = express();
-
 mongoose.connect(process.env.MONGODB_URI);
-
 mongoose.connection.on("connected", () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
   });
 
 const Dex = require('./models/pokedex.js')
 
-
-
 // Middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(methodOverride("_method")); 
 app.use(morgan("dev")); 
 
-// Routes
+//Controller functions
+const dexCtrl = require('./controllers/pokedex');
 
-app.get('/', async (req, res) => {
-    res.render('index.ejs');
-});
-
-app.get("/pokedex", async (req, res) => {
-    const seenPokemon = await Dex.find();
-    res.render("pokedex/index.ejs", { Dex: seenPokemon }
-    );
-});
-
-app.get('/pokedex/new', (req, res) => {
-    res.render("pokedex/new.ejs");
-});
-
-app.get("/pokedex/:DexId", async (req, res) => {
-    const pokemonDetailPage = await Dex.findById(req.params.DexId);
-    res.render(`pokedex/show.ejs`, { Dex: pokemonDetailPage });
-});
-
-app.post("/pokedex", async (req, res) => {
-    if (req.body.canEvolve === "on") {
-        req.body.canEvolve = true;
-    } else {
-        req.body.canEvolve = false;
-    }
-    await Dex.create(req.body);
-    res.redirect("/pokedex");
-});
-
-app.delete('/pokedex/:DexId', async (req, res) => {
-    await Dex.findByIdAndDelete(req.params.DexId);
-    res.redirect('/pokedex');
-});
-
-app.get("/pokedex/:DexId/edit", async (req, res) => {
-    const editEntry = await Dex.findById(req.params.DexId);
-    console.log(editEntry);
-    res.render("pokedex/edit.ejs", {
-        Dex: editEntry
-    });
-});
-
-app.put("/pokedex/:DexId", async (req, res) => {
-    if (req.body.canEvolve === "on") {
-        req.body.canEvolve = true;
-    } else {
-        req.body.canEvolve = false;
-    }
-    await Dex.findByIdAndUpdate(req.params.DexId, req.body);
-    res.redirect(`/pokedex/${req.params.DexId}`)
-});
-
+app.get('/', dexCtrl.home);
+app.get('/pokedex/new', dexCtrl.showNewForm);
+app.post('/pokedex', dexCtrl.create);
+app.get('/pokedex', dexCtrl.index);
+app.get('/pokedex/:DexId', dexCtrl.show);
+app.delete('/pokedex/:DexId', dexCtrl.remove);
+app.get('/pokedex/:DexId/edit', dexCtrl.edit);
+app.put('/pokedex/:DexId', dexCtrl.update);
 
 app.listen (3002, () => {
     console.log('Port 3002');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Routes pre MVC
+
+// app.get('/', async (req, res) => {
+//     res.render('index.ejs');
+// });
+
+// app.get("/pokedex", async (req, res) => {
+//     const seenPokemon = await Dex.find();
+//     res.render("pokedex/index.ejs", { Dex: seenPokemon }
+//     );
+// });
+
+// app.get('/pokedex/new', (req, res) => {
+//     res.render("pokedex/new.ejs");
+// });
+
+// app.get("/pokedex/:DexId", async (req, res) => {
+//     const pokemonDetailPage = await Dex.findById(req.params.DexId);
+//     res.render(`pokedex/show.ejs`, { Dex: pokemonDetailPage });
+// });
+
+// app.post("/pokedex", async (req, res) => {
+//     if (req.body.canEvolve === "on") {
+//         req.body.canEvolve = true;
+//     } else {
+//         req.body.canEvolve = false;
+//     }
+//     await Dex.create(req.body);
+//     res.redirect("/pokedex");
+// });
+
+
+// app.delete('/pokedex/:DexId', async (req, res) => {
+//     await Dex.findByIdAndDelete(req.params.DexId);
+//     res.redirect('/pokedex');
+// });
+
+// app.get("/pokedex/:DexId/edit", async (req, res) => {
+//     const editEntry = await Dex.findById(req.params.DexId);
+//     console.log(editEntry);
+//     res.render("pokedex/edit.ejs", {
+//         Dex: editEntry
+//     });
+// });
+
+// app.put("/pokedex/:DexId", async (req, res) => {
+//     if (req.body.canEvolve === "on") {
+//         req.body.canEvolve = true;
+//     } else {
+//         req.body.canEvolve = false;
+//     }
+//     await Dex.findByIdAndUpdate(req.params.DexId, req.body);
+//     res.redirect(`/pokedex/${req.params.DexId}`)
+// });
